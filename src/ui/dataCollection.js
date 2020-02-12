5a2cef236b29baf38958b147e4d4f7049d45cf9f
Harry <harrysmod69@gmail.com>
2020-02-12 17:32:28 +0000

    update and add training data

--- src/ui/dataCollection.js
+++ src/ui/dataCollection.js
@@ -16,7 +16,12 @@ class inputVideo {
         this.video = document.getElementById("video");
         this.video.src = src;
 
-        this.poseNet = ml5.poseNet(this.video, {inputResolution: 417}, callback);
+        this.poseNet = ml5.poseNet(this.video, {
+            inputResolution: 417,
+            outputStride: 32,
+            architecture: "ResNet50",
+            quantBytes: 4
+        }, callback);
         that = this;
         this.poseNet.on("pose", function (results) {
             that.gotPose(results); // I don't like using global video here but this scope gets lost :(
@@ -28,6 +33,15 @@ class inputVideo {
         this.canvas = document.getElementById("defaultCanvas0");
         this.ctx = this.canvas.getContext("2d");
 
+    }
+
+    setName(name){
+        this.name = name;
+    }
+
+    loadVideo(src, callback){
+        this.video.src = src;
+        this.video.onloadeddata = callback;
     }
 
     /**
@@ -147,41 +161,56 @@ class inputVideo {
 
 
 function addData(data, label) {
-    let d108 = data.slice(0, 108);
-    brain.addData(d108, [label]);
+    let sliced = data.slice(0, 250);
+    brain.addData(sliced, [label]);
 }
 
-function trainBrain(epochs = 50) {
+function trainBrain(epochs = 500) {
     brain.normalizeData();
     brain.train({"epochs":epochs}, () => {
         brain.save();
     });
 }
 
+let video
+
 function trainVideos(videos, exitFunc, i = 0) {
-    let video = new inputVideo(videos[i].name, videos[i].src, () =>{
+    video.data = [];
+    video.setName(videos[i].name);
+    video.loadVideo(videos[i].src, () => {
         video.startTraining(() => {
-            // All done
-            addData(video.data, video.name);
             i++;
-            if(i < videos.length){
+            addData(video.data, video.name);
+
+            if (i < videos.length) {
                 trainVideos(videos, exitFunc, i);
-            }else{
+            } else {
                 exitFunc();
             }
-
         });
     });
-
 }
 
-const videos = [
-    {"name": "rightWave", "src": "https://cdn.discordapp.com/attachments/366568880413343746/676387708968632321/received_537545687107800.mp4" },
-    {"name": "leftWave", "src": "https://cdn.discordapp.com/attachments/366568880413343746/676422482189746196/received_789329468242228.mp4" }
-];
+let videos = [];
+
+for (var i=1; i < 49; i++){
+    videos.push({
+        name: "leftWave",
+        src: "training/leftWave/left (" + i + ").mp4"
+    })
+}
+
+for (var i=1; i < 49; i++){
+    videos.push({
+        name: "rightWave",
+        src: "training/rightWave/right (" + i + ").mp4"
+    })
+}
 
 function compareVideo(i){
-    let video = new inputVideo(videos[i].name, videos[i].src, () => {
+    video.data = [];
+    video.setName(videos[i].name);
+    video.loadVideo(videos[i].src, () => {
         video.initClassify(() => {
             video.getFrameData((data) => {
                 video.classify(data, (confidence, label) => {
@@ -196,11 +225,15 @@ function setup() {
     brain = ml5.neuralNetwork({
         task: "classification",
         debug: true,
-        inputs: 108,
-        outputs: 2
+        inputs: 250,
+        outputs: 1
     });
+
     createCanvas(600, 640);
-   // trainVideos(videos,  trainBrain);
+
+    video = new inputVideo(videos[0].name, videos[0].src, () => {
+        //trainVideos(videos, trainBrain);
+    });
 }
 
 function draw() {
