6c498c414e2f36054008d309abc9e9e3aa9b73e0
Ben <bcrabbid@gmail.com>
2020-02-24 12:20:00 +0000

    Read and write other settings

--- src/json_reading/json_reader_writer.js
+++ src/json_reading/json_reader_writer.js
@@ -10,13 +10,43 @@ function getActionFromGesture(gestureName, status){
     } catch(err) {
         console.log(err);
     }
-    for(let i in bindings.gesture_action_bindings){
-        let binding = bindings.gesture_action_bindings[i];
+    for(let i in bindings.gesture_bindings){
+        let binding = bindings.gesture_bindings[i];
         if (binding.gesture === gestureName && binding.status === status){
             return binding;
         }
     }
     console.error("Error, no command found for this gesture");
+}
+
+function overwriteSetting(settingName, value){
+    let bindings;
+    try {
+        const jsonString = fs.readFileSync('src/json_reading/config.json');
+        bindings = JSON.parse(jsonString);
+    } catch(err) {
+        console.log(err);
+    }
+    bindings.general[settingName] = value;
+    var jsonContent = JSON.stringify(bindings);
+    fs.writeFile("src/json_reading/config.json", jsonContent, 'utf8', function (err) {
+        if (err) {
+            console.log("An error occurred while writing JSON Object to File.");
+            return console.log(err);
+        }
+
+        console.log("JSON file has been saved.");
+    });
+}
+function readSetting(settingName){
+    let bindings;
+    try {
+        const jsonString = fs.readFileSync('src/json_reading/config.json');
+        bindings = JSON.parse(jsonString);
+    } catch(err) {
+        console.log(err);
+    }
+    return bindings.general[settingName];
 }
 
 function overwriteAction(gestureName, status, newAction){
@@ -28,11 +58,20 @@ function overwriteAction(gestureName, status, newAction){
         console.log(err);
     }
     let newBinding ={"gesture": gestureName,"status": status, "action": newAction};
-    for(let i in bindings.gesture_action_bindings){
-        let binding = bindings.gesture_action_bindings[i];
+    for(let i in bindings.gesture_bindings){
+        let binding = bindings.gesture_bindings[i];
         if (binding.gesture === gestureName && binding.status === status){
-            bindings.gesture_action_bindings[i] = newBinding;
-            console.log(bindings.gesture_action_bindings[i]);
+            bindings.gesture_bindings[i] = newBinding;
+            console.log(bindings.gesture_bindings[i]);
+            var jsonContent = JSON.stringify(bindings);
+            fs.writeFile("src/json_reading/config.json", jsonContent, 'utf8', function (err) {
+                if (err) {
+                    console.log("An error occurred while writing JSON Object to File.");
+                    return console.log(err);
+                }
+
+                console.log("JSON file has been saved.");
+            });
         }
     }
 }
