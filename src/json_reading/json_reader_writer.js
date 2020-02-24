7936d2b1928b7872b31e70a0d502206cd1b5d43d
Ben <bcrabbid@gmail.com>
2020-02-24 11:57:26 +0000

    got read & write working

--- src/json_reading/json_reader_writer.js
+++ src/json_reading/json_reader_writer.js
@@ -1,45 +1,39 @@
-const config = require('./config.json');
 const fs = require('fs');
 
 
-function getActionFromGesture( gestureName, status){
+function getActionFromGesture(gestureName, status){
     let bindings;
     try {
-        const jsonString = fs.readFileSync('./config.json');
-        let bindings = JSON.parse(jsonString);
+        const jsonString = fs.readFileSync('src/json_reading/config.json');
+        bindings = JSON.parse(jsonString);
+        console.log(bindings);
     } catch(err) {
         console.log(err);
     }
-
-    for(i = 0; i < bindings.length; i++){
-        if (bindings.gesture_action_bindings[i].gesture === gestureName &&
-            bindings.gesture_action_bindings[i].status === status){
-            break;
+    for(let i in bindings.gesture_action_bindings){
+        let binding = bindings.gesture_action_bindings[i];
+        if (binding.gesture === gestureName && binding.status === status){
+            return binding;
         }
-        if (i == binding.length) {
-            console.error("Error, no command found for this gesture");
-        }
-}
+    }
+    console.error("Error, no command found for this gesture");
 }
 
 function overwriteAction(gestureName, status, newAction){
     let bindings;
     try {
-        const jsonString = fs.readFileSync('./config.json');
+        const jsonString = fs.readFileSync('src/json_reading/config.json');
         bindings = JSON.parse(jsonString);
     } catch(err) {
         console.log(err);
     }
     let newBinding ={"gesture": gestureName,"status": status, "action": newAction};
-
-    for(i = 0; i < bindings.length; i++){
-        if (bindings.gesture_action_bindings[i].gesture === gestureName &&
-            bindings.gesture_action_bindings[i].status === status){
-            break;
+    for(let i in bindings.gesture_action_bindings){
+        let binding = bindings.gesture_action_bindings[i];
+        if (binding.gesture === gestureName && binding.status === status){
+            bindings.gesture_action_bindings[i] = newBinding;
+            console.log(bindings.gesture_action_bindings[i]);
         }
-        bindings.splice(i,1);
-        bindings.insert(i,newBinding);
-
-}
+    }
 }
 
